1a56a54653b4013e195730a9ca51677ec7439e3b
Cindy Maria Thewes <78299739+cimth@users.noreply.github.com>
2021-03-11 16:37:26 +0100

    Initialize basic Spring Boot project

---
+++ src/main/java/com/example/quizmaster_backend/QuizmasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizmaster_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmasterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizmasterBackendApplication.class, args);
+    }
+
+}
